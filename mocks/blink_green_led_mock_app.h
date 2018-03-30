a47e4885298c9636989e56625b0fb6fcd95d32c1
narasimhaugra <narasimhaugra@gmail.com>
2018-03-30 09:15:14 +0530

    blinkk led

---
+++ mocks/blink_green_led_mock_app.h
@@ -0,0 +1,27 @@
+/**
+ * @file blink_green_led_mock_app.h
+ * Mock (gmock) of blink_green_led app.
+ *
+ * @section LICENSE
+ * [Standard license info.]
+ *
+ * Copyright 2014 Gram Power, Inc.
+ */
+
+#ifndef BLINK_GREEN_LED_MOCK_APP_H_
+#define BLINK_GREEN_LED_MOCK_APP_H_
+
+#include "gmock/gmock.h"
+#include "../blink_green_led_api.h"
+
+
+
+class BlinkGreenLedMockApp : BlinkGreenLed_API
+{
+
+public:
+    // MOCK METHODS GO HERE
+};
+
+
+#endif  /** BLINK_GREEN_LED_MOCK_APP_H_ */
